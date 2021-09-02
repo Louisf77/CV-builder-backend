0f36df6f3c9d7163b27c173488392be2f72e5348
Louis Freeman <louis.freeman@gmail.com>
2021-09-02 16:24:58 +0100

    add membership test

--- server.ts
+++ server.ts
@@ -19,39 +19,49 @@ app.use(cors())
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from users');
-  res.json(dbres.rows);
+app.get("/:sub", async (req, res) => {
+  const sub = req.params.sub
+  try {
+    const dbres = await client.query('select * from users where sub = $1',[sub]);
+    return res.json(dbres.rowCount);
+  } catch (error) {
+    console.error(error.message)
+  }
+  
 });
 
 //view cv for specific user
-app.get("/viewCV/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.get("/viewCV/:sub", async (req,res) => {
+  const sub = req.params.sub
   try {
-    const queryValues = [userID]
-    const userQuery = "select * from users where user_id = $1"
+    const queryValues = [sub]
+    const userQuery = "select * from users where sub = $1"
     const userQRes = await client.query(userQuery,queryValues)
     let user = userQRes.rows[0]
 
-    const edQuery = "select ed_id,institution_name,start_date,end_date,qualification_level,grade,subject from education where user_id = $1 order by end_date desc"
+    const edQuery = "select ed_id,institution_name,start_date,end_date,qualification_level,grade,subject from education where sub = $1 order by end_date desc"
     const edQRes = await client.query(edQuery,queryValues)
     user.education = edQRes.rows
 
-    const workQuery = "select work_id,company_name,role,start_date,end_date,responsibilities from work_experience where user_id = $1 order by end_date desc"
+    const workQuery = "select work_id,company_name,role,start_date,end_date,responsibilities from work_experience where sub = $1 order by end_date desc"
     const workQRes = await client.query(workQuery,queryValues)
     user.work = workQRes.rows
 
-    const skillQuery = "select skill_id,skill from skills where user_id = $1"
+    const skillQuery = "select skill_id,skill from skills where sub = $1"
     const skillQRes = await client.query(skillQuery,queryValues)
     user.skill = skillQRes.rows
 
-    const interestQuery = "select interest_id,interest from interests where user_id = $1"
+    const interestQuery = "select interest_id,interest from interests where sub = $1"
     const interestQRes = await client.query(interestQuery,queryValues)
     user.interest = interestQRes.rows
 
-    const bioQuery = "select bio_id,bio from bio where user_id = $1"
+    const bioQuery = "select bio_id,bio from bio where sub = $1"
     const bioQRes = await client.query(bioQuery,queryValues)
     user.bio = bioQRes.rows
+
+    const softwareQuery = "select software_id,software from software where sub = $1"
+    const softwareQRes = await client.query(softwareQuery,queryValues)
+    user.software = softwareQRes.rows
     
     res.status(201).json({
       status: "success",
@@ -88,11 +98,11 @@ app.post("/create/personal-details", async (req,res) => {
 })
 
  // view education for specific user
-app.get("/education/:userID", async (req, res) => {
-  const userID = req.params.userID
+app.get("/education/:sub", async (req, res) => {
+  const sub = req.params.userID
   try {
-    const queryValues = [userID]
-    const query = "SELECT * from education where user_id = $1"
+    const queryValues = [sub]
+    const query = "SELECT * from education where sub = $1"
     const dbres = await client.query(query,queryValues);
     res.json(dbres.rows)
   } catch (error) {
@@ -103,17 +113,17 @@ app.get("/education/:userID", async (req, res) => {
 
 
 //create education details for cv
-app.post("/create/education/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.post("/create/education/:sub", async (req,res) => {
+  const sub = req.params.sub
   const {institutionName,startDate,endDate,qualificationLevel,grade,subject} = req.body
   try {
-    const queryValues = [userID,institutionName,startDate,endDate,qualificationLevel,grade,subject]
-    const query = "INSERT into education(user_id,institution_name,start_date,end_date,qualification_level,grade,subject) values ($1,$2,$3,$4,$5,$6,$7)"
+    const queryValues = [sub,institutionName,startDate,endDate,qualificationLevel,grade,subject]
+    const query = "INSERT into education(sub,institution_name,start_date,end_date,qualification_level,grade,subject) values ($1,$2,$3,$4,$5,$6,$7)"
     await client.query(query,queryValues)
     res.status(201).json({
       status: "success",
       data:{
-        message: `Added "${userID}'s education to their CV'`
+        message: `Added "${sub}'s education to their CV'`
       }
     })
   } catch (error) {
@@ -123,11 +133,11 @@ app.post("/create/education/:userID", async (req,res) => {
 
 
  // view work for specific user
- app.get("/work/:userID", async (req, res) => {
-  const userID = req.params.userID
+ app.get("/work/:sub", async (req, res) => {
+  const sub = req.params.sub
   try {
-    const queryValues = [userID]
-    const query = "SELECT * from work_experience where user_id = $1"
+    const queryValues = [sub]
+    const query = "SELECT * from work_experience where sub = $1"
     const dbres = await client.query(query,queryValues);
     res.json(dbres.rows)
   } catch (error) {
@@ -138,17 +148,17 @@ app.post("/create/education/:userID", async (req,res) => {
 
 
 //create work details for cv
-app.post("/create/work/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.post("/create/work/:sub", async (req,res) => {
+  const sub = req.params.sub
   const {companyName,role,startDate,endDate,responsibilities} = req.body
   try {
-    const queryValues = [userID,companyName,role,startDate,endDate,responsibilities]
-    const query = "INSERT into work_experience(user_id,company_name,role,start_date,end_date,responsibilities) values ($1,$2,$3,$4,$5,$6)"
+    const queryValues = [sub,companyName,role,startDate,endDate,responsibilities]
+    const query = "INSERT into work_experience(sub,company_name,role,start_date,end_date,responsibilities) values ($1,$2,$3,$4,$5,$6)"
     await client.query(query,queryValues)
     res.status(201).json({
       status: "success",
       data:{
-        message: `Added "${userID}'s work experience to their CV'`
+        message: `Added "${sub}'s work experience to their CV'`
       }
     })
   } catch (error) {
@@ -157,11 +167,11 @@ app.post("/create/work/:userID", async (req,res) => {
 })
 
 // view skills for specific user
-app.get("/skills/:userID", async (req, res) => {
-  const userID = req.params.userID
+app.get("/skills/:sub", async (req, res) => {
+  const sub = req.params.sub
   try {
-    const queryValues = [userID]
-    const query = "SELECT * from skills where user_id = $1"
+    const queryValues = [sub]
+    const query = "SELECT * from skills where sub = $1"
     const dbres = await client.query(query,queryValues);
     res.json(dbres.rows)
   } catch (error) {
@@ -172,17 +182,17 @@ app.get("/skills/:userID", async (req, res) => {
 
 
 //create skills for cv
-app.post("/create/skills/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.post("/create/skills/:sub", async (req,res) => {
+  const sub = req.params.sub
   const {skill} = req.body
   try {
-    const queryValues = [userID,skill]
-    const query = "INSERT into skills(user_id,skill) values ($1,$2)"
+    const queryValues = [sub,skill]
+    const query = "INSERT into skills(sub,skill) values ($1,$2)"
     await client.query(query,queryValues)
     res.status(201).json({
       status: "success",
       data:{
-        message: `Added "${userID}'s skills to their CV'`
+        message: `Added "${sub}'s skills to their CV'`
       }
     })
   } catch (error) {
@@ -191,11 +201,11 @@ app.post("/create/skills/:userID", async (req,res) => {
 })
 
 // view interests for specific user
-app.get("/interests/:userID", async (req, res) => {
-  const userID = req.params.userID
+app.get("/interests/:sub", async (req, res) => {
+  const sub = req.params.sub
   try {
-    const queryValues = [userID]
-    const query = "SELECT * from interests where user_id = $1"
+    const queryValues = [sub]
+    const query = "SELECT * from interests where sub = $1"
     const dbres = await client.query(query,queryValues);
     res.json(dbres.rows)
   } catch (error) {
@@ -206,17 +216,17 @@ app.get("/interests/:userID", async (req, res) => {
 
 
 //create skills for cv
-app.post("/create/interests/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.post("/create/interests/:sub", async (req,res) => {
+  const sub = req.params.sub
   const {interest} = req.body
   try {
-    const queryValues = [userID,interest]
-    const query = "INSERT into interests(user_id,interest) values ($1,$2)"
+    const queryValues = [sub,interest]
+    const query = "INSERT into interests(sub,interest) values ($1,$2)"
     await client.query(query,queryValues)
     res.status(201).json({
       status: "success",
       data:{
-        message: `Added "${userID}'s interests to their CV'`
+        message: `Added "${sub}'s interests to their CV'`
       }
     })
   } catch (error) {
@@ -225,11 +235,11 @@ app.post("/create/interests/:userID", async (req,res) => {
 })
 
 // view bio for specific user
-app.get("/bio/:userID", async (req, res) => {
-  const userID = req.params.userID
+app.get("/bio/:sub", async (req, res) => {
+  const sub = req.params.sub
   try {
-    const queryValues = [userID]
-    const query = "SELECT * from bio where user_id = $1"
+    const queryValues = [sub]
+    const query = "SELECT * from bio where sub = $1"
     const dbres = await client.query(query,queryValues);
     res.json(dbres.rows)
   } catch (error) {
@@ -240,17 +250,17 @@ app.get("/bio/:userID", async (req, res) => {
 
 
 //create bio for cv
-app.post("/create/bio/:userID", async (req,res) => {
-  const userID = req.params.userID
+app.post("/create/bio/:sub", async (req,res) => {
+  const sub = req.params.sub
   const {bio} = req.body
   try {
-    const queryValues = [userID,bio]
-    const query = "INSERT into bio(user_id,bio) values ($1,$2)"
+    const queryValues = [sub,bio]
+    const query = "INSERT into bio(sub,bio) values ($1,$2)"
     await client.query(query,queryValues)
     res.status(201).json({
       status: "success",
       data:{
-        message: `Added "${userID}'s bio to their CV'`
+        message: `Added "${sub}'s bio to their CV'`
       }
     })
   } catch (error) {
@@ -258,6 +268,23 @@ app.post("/create/bio/:userID", async (req,res) => {
   }
 })
 
+app.post("/create/software/:sub", async (req,res) => {
+  const sub = req.params.sub
+  const {software} = req.body
+  try {
+    const queryValues = [sub,software]
+    const query = "INSERT into software(sub,software) values ($1,$2)"
+    await client.query(query,queryValues)
+    res.status(201).json({
+      status: "success",
+      data:{
+        message: `Added "${sub}'s software skills to their CV'`
+      }
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+})
 
 //Start the server on the given port
 const port = process.env.PORT;
